de20ff5fc0262899a4fe4dbfbd6480b647d76e76
Riley Martine <riley.martine@protonmail.com>
2016-05-16 15:03:20 -0400

    add scpe, change things

--- 10-little-things.sh
+++ 10-little-things.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-diff --suppress-common-lines <(sed 's/ .*//' /home/riley/Documents/filesystem.manifest) <(apt-mark showmanual | sort | uniq) | grep '>' | sed 's/[>] //' > /home/riley/backups/packages.txt
+diff --suppress-common-lines <(sed 's/ .*//' ~/Documents/filesystem.manifest) <(apt-mark showmanual | sort | uniq) | grep '>' | sed 's/[>] //' > ~/backups/packages.txt
 
-cp /etc/hosts /home/riley/backups
-cp /home/riley/.gitconfig /home/riley/backups
-cp /home/riley/.zsh* /home/riley/backups
-cp /etc/fstab /home/riley/backups
+cp /etc/hosts ~/backups
+cp ~/.gitconfig ~/backups
+cp ~/.zsh* ~/backups
+cp ~/.bash* ~/backups
+cp /etc/fstab ~/backups
 
-cp -r /home/riley/.oh-my-zsh/ /home/riley/backups
-cp -r /home/riley/Documents/ /home/riley/backups
-cp -r /usr/share/zsh /home/riley/backups/usr/share
-cp -r /home/riley/Tools/ /home/riley/backups
+cp -r ~/.oh-my-zsh/ ~/backups
+cp -r ~/Documents/ ~/backups
+cp -r /usr/share/zsh ~/backups/usr/share
+cp -r ~/Tools/ ~/backups
 
