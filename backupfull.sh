60f2b86ba9d6c7e06c8238ff85a1ee1537e2f238
Riley Martine <riley.martine@protonmail.com>
2016-05-16 15:06:29 -0400

    remove old things

--- backupfull.sh
+++ backupfull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -aAX --info=progress2 --delete --exclude={"/etc/fstab","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*","/home/*/.gvfs","/home/riley/.steam/Steam/steamapps/*","/home/riley/Desktop/Ex\ Machina/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /media/riley/INFECTED
+rsync -aAX --info=progress2 --delete --exclude={"/etc/fstab","/home/*/.cache/chromium/*","/home/*/.local/share/Trash/*","/home/*/.gvfs","/home/*/.steam/Steam/steamapps/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /media/riley/INFECTED
