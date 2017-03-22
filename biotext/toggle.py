dcdc9192461de934bac26f9f8362431959c876bd
riley <riley.martine@protonmail.com>
2017-03-22 09:41:27 -0400

    Add bottom terminal

---
+++ biotext/toggle.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python2
+import gtk.gdk
+w = gtk.gdk.window_foreign_new( gtk.gdk.get_default_root_window().property_get("_NET_ACTIVE_WINDOW")[2][0] )
+w.set_decorations( (w.get_decorations()+1)%2 ) # toggle between 0 and 1
+gtk.gdk.window_process_all_updates()
+gtk.gdk.flush()
